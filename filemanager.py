ab4c8d4d897aaa498f5085ae831ab5de6b8d20d0
josh-swz <josh@specwerkz.com>
2020-11-29 00:36:44 -0800

    Write and Read Files practice

--- filemanager.py
+++ filemanager.py
@@ -1,5 +1,12 @@
-x = input('Type whatever you want')
+from time import sleep
+
+x = input('Type whatever you want: ')
 
 with open('file.txt', 'w') as myFile:
-    myFile.write('Typing...')
-    
+    myFile.write(x)
+
+sleep(1)
+
+with open('file.txt', 'r') as myFile:
+    print(myFile.read())
+
